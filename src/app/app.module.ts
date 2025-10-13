1eb05a0378bd867d5c3a9559f47f8217fbf99043
PanBec <panbec@wp.pl>
2025-10-13 20:25:52 +0200

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BannerComponent } from './banner/banner.component';
+import { AboutComponent } from './about/about.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { LatestComponent } from './latest/latest.component';
+import { LiveSessionComponent } from './live-session/live-session.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterPlayerComponent } from './footer-player/footer-player.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BannerComponent,
+    AboutComponent,
+    CategoriesComponent,
+    LatestComponent,
+    LiveSessionComponent,
+    ContactComponent,
+    FooterPlayerComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/footer-player/footer-player.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-player',
+  templateUrl: './footer-player.component.html',
+  styleUrl: './footer-player.component.scss'
+})
+export class FooterPlayerComponent {
+
+}
